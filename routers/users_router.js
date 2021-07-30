96d409b5eeb85a169ea03e4c0dd5b164b929e382
gzh236 <47617174+gzh236@users.noreply.github.com>
2021-07-31 02:08:58 +0800

    added multer

--- routers/users_router.js
+++ routers/users_router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
